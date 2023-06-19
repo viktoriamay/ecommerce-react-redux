a4bafe3543459e690a113da0b498fc5ababaabe4
viktoriamay <alterkelly@mail.ru>
2023-06-19 18:45:59 +0300

    Add Main

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import './Main.scss';
+import React from 'react';
+
+export const Main = () => {
+  return (
+    <section className="main_page">
+      <h1 className="main_page__title">Fall collection is out here</h1>
+    </section>
+  );
+};
